c5e2ead0baa4695686624256a2c24922d06e2be4
Roger Rojas <rrojas@indracompany.com>
2021-02-19 16:50:03 -0500

    fix(main): cambiando tag de imagen python y agregando control de errores al main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from sys import argv
 
 if __name__ == '__main__':
     # print(argv[1])
-    if len(argv) == 1:
+    try:
         db_service = DBService(argv[1])
         db_service.run()
-    else:
+    except IndexError as e:
         print("Ejecutar el comando con el siguiente formato: python main.py <dbbname>")
